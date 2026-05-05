1dde0bd5ca5d023690602b6fe592063153ff25c1
ludwiglindermann <lu.lindermann@duocuc.cl>
2026-05-04 22:07:24 -0400

    Corrige prueba de contexto para wallet local

--- src/test/java/com/duoc/mascotasordenes/MascotasordenesApplicationTests.java
+++ src/test/java/com/duoc/mascotasordenes/MascotasordenesApplicationTests.java
@@ -2,12 +2,16 @@ package com.duoc.mascotasordenes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:oracle:thin:@basefullstack_high?TNS_ADMIN=C:/OracleWallet",
+    "spring.jpa.hibernate.ddl-auto=none"
+})
 class MascotasordenesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
